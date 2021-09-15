5491e6fc3c3ce7aa499840271b754433e4304600
kenannn <kenanjoshua3454@gmail.com>
2021-09-16 06:01:01 +0800

    sidebar List Event Listener

--- src/index.js
+++ src/index.js
@@ -3,36 +3,35 @@ const path = require('path');
 const { electron } = require('process');
 const ipc = require('electron').ipcMain;
 
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+if (require('electron-squirrel-startup')) {
+	// eslint-disable-line global-require
+	app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences:{
-      nodeIntegration: true,
-      contextIsolation: false,
-      enableRemoteModule: true,
-    }
-  });
+	// Create the browser window.
+	const mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+			enableRemoteModule: true,
+		},
+	});
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+	// and load the index.html of the app.
+	mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+	// Open the DevTools.
+	mainWindow.webContents.openDevTools();
 };
 
-
-ipc.on('open-error-dialog', function(event){
-  dialog.showErrorBox("Erorr", "Gakdog");
-  console.log("REACHED");
-})
+ipc.on('open-error-dialog', function (event) {
+	dialog.showErrorBox('Erorr', 'Gakdog');
+	console.log('REACHED');
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -43,17 +42,17 @@ app.on('ready', createWindow);
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
 });
 
 // In this file you can include the rest of your app's specific main process
--- src/render.js
+++ src/render.js
@@ -1,54 +1,72 @@
 const ipc = require('electron').ipcRenderer;
 const exec = require('child_process').execSync;
 
+const sidebar = document.getElementById('sidebar');
+const main = document.getElementById('display');
+const displayList = document.getElementById('paths');
 
-const sidebar = document.getElementById("sidebar");
-const main = document.getElementById("display");
+let fileList = [];
+let queryFileList = [];
 
 // const dropArea = document.querySelector("#sideline");
 
+function updateQueryFileList() {
+	for (let i = queryFileList.length; i < fileList.length; i++) {
+		queryFileList[i] = document.getElementById(i);
+		queryFileList[i].addEventListener('click', (event) => {
+			alert('CLIKEDDDD');
+			let output = '';
+			main.style.textAlign = 'left';
+			output = exec('strings "' + fileList[i] + '"', {
+				encoding: 'utf-8',
+			});
+			output = output.replaceAll('\n', '<br>');
+			main.innerHTML = output;
+		});
+	}
+}
 
 sidebar.addEventListener('drop', (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    let output = ""
-    for (const f of event.dataTransfer.files) {
-        // Using the path attribute to get absolute file path
-        main.style.textAlign = "left";
-        console.log(f.path);
-        console.log("strings " + f.path);
-        output = exec("strings \"" + f.path + "\"", { encoding: 'utf-8' });
-        console.log(output); 
-    }
-    output = output.replaceAll("\n", "<br>");
-    console.log(output);
-    main.innerHTML = output;
-});
-  
-sidebar.addEventListener('dragover', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-  });
-  
-sidebar.addEventListener('dragenter', (event) => {
-    console.log('File is in the Drop Space');
-});
-  
-sidebar.addEventListener('dragleave', (event) => {
-    console.log('File has left the Drop Space');
+	event.preventDefault();
+	event.stopPropagation();
+
+	for (const f of event.dataTransfer.files) {
+		fileList.push(f.path);
+	}
+
+	cleanName = fileList[fileList.length - 1].split('\\');
+	cleanName = cleanName[cleanName.length - 1];
+	let node = document.createElement('li');
+	let textnode = document.createTextNode(cleanName);
+	node.appendChild(textnode);
+	node.setAttribute('id', fileList.length - 1);
+	displayList.appendChild(node);
+	updateQueryFileList();
 });
 
+sidebar.addEventListener('dragover', (e) => {
+	e.preventDefault();
+	e.stopPropagation();
+});
+
+sidebar.addEventListener('dragenter', (event) => {
+	console.log('File is in the Drop Space');
+});
+
+sidebar.addEventListener('dragleave', (event) => {
+	console.log('File has left the Drop Space');
+});
 
 // function getStrings(fileName) {
 // 	exec("strings " + fileName, (err, stdout, stderr) => console.log(stdout));
 // }
 
-
-function mainDisplay(){
-    exec(cmd, (err, stdout) => 
-        document.querySelector(".main").innerHTML = (stdout),
-    );
-    console.log("Main DIsplay Called")
+function mainDisplay() {
+	exec(
+		cmd,
+		(err, stdout) => (document.querySelector('.main').innerHTML = stdout)
+	);
+	console.log('Main DIsplay Called');
 }
 
 // mainDisplay();
